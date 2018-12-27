fbaae0cf0ead91a59c2ab16579386e2eec3a69d9
Cheng Guodong <chengguodong@growingio.com>
2018-12-27 16:53:54 +0800

    修复 scala-compiler 安全问题

--- src/main/scala/com/gordon/algorithm/sort/other_sort/MergeSort.scala
+++ src/main/scala/com/gordon/algorithm/sort/other_sort/MergeSort.scala
@@ -1,8 +1,18 @@
 package com.gordon.algorithm.sort.other_sort
 
+import com.gordon.algorithm.sort.Sort
+
 /**
  * 归并排序
  */
-class MergeSort {
+class MergeSort extends Sort {
+  override def name: String = "两路归并排序"
 
+  override def sort(arr: Array[Int]): Unit = {
+    sort(arr, 0, arr.length)
+  }
+
+  def sort(arr: Array[Int], low: Int, high: Int): Unit = {
+    val mid = (low + high) / 2
+  }
 }
